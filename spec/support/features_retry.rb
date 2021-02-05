f34ad7a50f401a735a79f2e8451daaecfcda9e8f
sebi <gore.sebyx@yahoo.com>
2021-02-05 23:01:32 +0000

    fix

--- spec/support/features_retry.rb
+++ spec/support/features_retry.rb
@@ -4,4 +4,10 @@ RSpec.configure do |config|
   config.around :each, :feature do |ex|
     ex.run_with_retry retry: 3
   end
+
+  config.retry_callback = proc do |ex|
+    if ex.metadata[:type] == :feature
+      Capybara.reset!
+    end
+  end
 end
