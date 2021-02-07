b283391b7d0a4d9e3c6db794c09b90330f4c3670
sebi <gore.sebyx@yahoo.com>
2021-02-07 17:02:49 +0000

    update

--- app/services/setup_service/download_cities.rb
+++ app/services/setup_service/download_cities.rb
@@ -2,7 +2,7 @@ module SetupService
   class DownloadCities
     def call!
       `wget https://s3-us-west-2.amazonaws.com/cities-gem/cities.tar.gz -O /tmp/cities.tar.gz`
-      `tar -xzf /tmp/cities.tar.gz -C #{Rails.root.join("vendor/cities")}`
+      `tar -xzf /tmp/cities.tar.gz -C #{Rails.root.join("vendor")}`
     end
   end
 end
