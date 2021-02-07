99cb79c7d307c7e04219e8b425296b9e8555cb9a
sebi <gore.sebyx@yahoo.com>
2021-02-07 21:08:41 +0000

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,12 @@ module.exports = {
   purge: {
     enabled: process.env.RAILS_ENV === 'production',
     content: [
-      './app/snowpacker/**/*.jsx',
+      './app/javascript/**/*.js',
+      './app/javascript/**/*.jsx',
       './app/assets/javascripts/**/*.js',
       './app/views/**/*.html.erb',
       './app/components/**/*.rb',
+      './app/components/**/*.html.erb',
       './app/helpers/**/*.rb',
     ],
   },
