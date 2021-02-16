18543185822487e7703ec4b5ec5010439718445f
sebi <gore.sebyx@yahoo.com>
2021-02-16 22:00:07 +0000

    update

--- spec/workers/peer_info_worker/ping_spec.rb
+++ spec/workers/peer_info_worker/ping_spec.rb
@@ -4,6 +4,7 @@ require "rails_helper"
 
 describe PeerInfoWorker::Ping, vcr: { record: :once } do
   let(:peer_info) { create(:peer_info, ip: "161.97.64.223", friend_ship_status: :accepted) }
+  let(:requests) { subject.requests }
 
   subject do
     described_class.new.perform(true)
@@ -12,5 +13,10 @@ describe PeerInfoWorker::Ping, vcr: { record: :once } do
   it "pings the server" do
     peer_info
     subject
+
+    expect(requests.size).to eq 1
+    requests.each do |r|
+      subject.check_response(r)
+    end
   end
 end
