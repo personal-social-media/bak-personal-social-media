d779dab07cfd1ac5d070d9e47892ef0260ab3f64
sebi <gore.sebyx@yahoo.com>
2021-04-04 21:39:49 +0100

    change

--- app/services/documentation_service/documentation_record.rb
+++ app/services/documentation_service/documentation_record.rb
@@ -19,103 +19,111 @@ module DocumentationService
       end
     end
 
-    def build_result
-      return if result.present?
-      @result = {
-        parent: {
-          id: documentation_parent_id,
-          controller: controller,
-          title: documentation_parent_title,
-          usage: documentation_usage
-        },
-        child:  {
-          id: documentation_id,
-          title: documentation_title,
-          information: documentation_information || documentation_title,
-          params: format_saved_hash(documentation_params),
-          unescaped_url: documentation_unescaped_url,
-          url: url,
-          method: request.method,
-          response_code: response.status,
-          body: body,
-          json: json.nil? ? response.body : format_saved_hash(json)
+    private
+      def build_result
+        return if result.present?
+        @result = {
+          parent: {
+            id: documentation_parent_id,
+            controller: controller,
+            title: documentation_parent_title,
+            usage: documentation_usage
+          },
+          child:  {
+            id: documentation_id,
+            title: documentation_title,
+            information: documentation_information || documentation_title,
+            params: format_saved_hash(documentation_params),
+            unescaped_url: documentation_unescaped_url,
+            url: url,
+            method: request.method,
+            response_code: response.status,
+            body: body,
+            json: json.nil? ? response.body : format_saved_hash(json),
+            filters: filters
+          }
         }
-      }
-    end
-
-    def body
-      b = request.body.read
-      return b if b.blank?
-      format_saved_hash(params)
-    end
-
-    def directory
-      @directory ||= Rails.root.join("spec/documentation/#{documentation_parent_id}").to_s
-    end
-
-    def format_saved_hash(hash)
-      return hash if hash.blank?
-      hash.deep_transform_values do |value|
-        if is_uid?(value)
-          next "UID"
-        end
-        if is_signature(value)
-          next "SIGNATURE"
-        end
-
-        if is_path?(value)
-          next "file_path"
-        end
-
-        if is_date?(value)
-          next "2020-01-01T00:00:00+00:00"
-        end
-
-        value
       end
-    end
 
-    def is_date?(value)
-      return false unless value.is_a?(String)
-      return false if integer?(value)
-      Date.parse(value)
-    rescue Exception
-      false
-    end
-
-    def integer?(value)
-      /\A[+-]?\d+\z/.match?(value)
-    end
-
-    def is_path?(value)
-      return false unless value.is_a?(String)
-      value.match(/\/tmp/)
-    end
-
-    def is_uid?(value)
-      return false unless value.is_a?(String)
-      value.match?(/UID/)
-    end
-
-    def is_signature(value)
-      return false unless value.is_a?(String)
-      value.match(/===/)
-    end
-
-    def file_path
-      directory + "/#{documentation_id}.json"
-    end
-
-    def write_result
-      File.open(file_path, "w") do |f|
-        f.write(JSON.pretty_generate(result))
+      def filters
+        filters = request.controller_class.__callbacks[:process_action].map { |action| action.instance_variable_get("@key") }
+        filters.select! { |f| f.is_a?(Symbol) }
+        filters - %i(use_react_component_helper verify_same_origin_request)
       end
-    end
 
-    def method_missing(symbol, *_args)
-      spec_ctx.instance_exec { self.send(symbol) }
-    rescue NoMethodError
-      nil
-    end
+      def body
+        b = request.body.read
+        return b if b.blank?
+        format_saved_hash(params)
+      end
+
+      def directory
+        @directory ||= Rails.root.join("spec/documentation/#{documentation_parent_id}").to_s
+      end
+
+      def format_saved_hash(hash)
+        return hash if hash.blank?
+        hash.deep_transform_values do |value|
+          if is_uid?(value)
+            next "UID"
+          end
+          if is_signature(value)
+            next "SIGNATURE"
+          end
+
+          if is_path?(value)
+            next "file_path"
+          end
+
+          if is_date?(value)
+            next "2020-01-01T00:00:00+00:00"
+          end
+
+          value
+        end
+      end
+
+      def is_date?(value)
+        return false unless value.is_a?(String)
+        return false if integer?(value)
+        Date.parse(value)
+      rescue Exception
+        false
+      end
+
+      def integer?(value)
+        /\A[+-]?\d+\z/.match?(value)
+      end
+
+      def is_path?(value)
+        return false unless value.is_a?(String)
+        value.match(/\/tmp/)
+      end
+
+      def is_uid?(value)
+        return false unless value.is_a?(String)
+        value.match?(/UID/)
+      end
+
+      def is_signature(value)
+        return false unless value.is_a?(String)
+        value.match(/===/)
+      end
+
+      def file_path
+        directory + "/#{documentation_id}.json"
+      end
+
+      def write_result
+        File.open(file_path, "w") do |f|
+          f.write(JSON.pretty_generate(result))
+        end
+      end
+
+      def method_missing(symbol, *_args)
+        spec_ctx.instance_exec { self.send(symbol) }
+      rescue NoMethodError
+        nil
+      end
   end
 end
