31c0328e9d5478e778d76b5f3d50c9f287426914
sebi <gore.sebyx@yahoo.com>
2021-04-05 13:18:23 +0100

    fix

--- spec/support/documentation.rb
+++ spec/support/documentation.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
-  config.before(documentation: true) do
+  config.before(documentation: true, valid: true) do
     ActiveRecord::Base.connection.tables.each do |t|
       ActiveRecord::Base.connection.reset_pk_sequence!(t)
     end
+    FactoryBot.rewind_sequences
   end
 
   config.after(documentation: true, valid: true) do |ex|
